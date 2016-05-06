cb216707b5fe57f7e81a1054d39e7bbfd4553c37
kulnor <pascal.heus@gmail.com>
2016-05-06 08:14:47 +0200

    Added license text to source code

--- ImageMagickIdentifyParser.py
+++ ImageMagickIdentifyParser.py
@@ -1,4 +1,37 @@
 #!/usr/bin/python
+#
+#  Utility class to parse the ImageMagick identify -verbose <image> command
+#  and convert to various formats for further reuse by code or applications.
+# 
+# 
+# Copyright (c) 2016, Metadata Technology North America Inc. (http://www.mtna.us)
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+# 
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# 
+# * Neither the name of imagemagick-identify-parser nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# 
 import distutils.spawn
 import json
 import os
@@ -385,7 +418,7 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description='ImageMagick identify -verbose parser and convertor')
     parser.add_argument("filename", help="The input file")
-    parser.add_argument('--type' , '-t',default='json', help='The type of output. Can be json|irods|raw|xml.')
+    parser.add_argument('--type' , '-t',default='json', help='The output type. Can be json|irods|raw|xml.')
     args = parser.parse_args()
     
     o = ImageMagickIdentifyParser()
@@ -401,4 +434,4 @@ if __name__ == '__main__':
         print o.toXML()
     else:
         print "Invalid type specified:" + args.type
-	   
+# 	   
